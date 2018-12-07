d53918aac53e22be317b1856ffd3a7e29f89a252
Chris Morgan <chris.morgan@perspectum-diagnostics.com>
2018-12-07 15:56:24 +0000

    Initial add of ASG, ASG schedule and EFS

---
+++ asg.tf
@@ -0,0 +1,34 @@
+# --------------------------
+# Jenkins Auto-Scaling Group
+# --------------------------
+resource "aws_autoscaling_group" "jenkins" {
+  depends_on = ["aws_efs_file_system.this"]
+  depends_on = ["aws_launch_configuration.jenkins"]
+
+  name                      = "${var.environment}_jenkins_asg"
+  max_size                  = "${var.max_size}"
+  min_size                  = "${var.min_size}"
+  desired_capacity          = "${var.desired_capacity}"
+  launch_configuration      = "${aws_launch_configuration.jenkins.name}"
+  vpc_zone_identifier       = ["${var.vpc_zone_identifier}"]
+  health_check_grace_period = "${var.health_check_grace_period}"
+  health_check_type         = "${var.health_check_type}"
+
+  tag {
+    key                 = "Name"
+    value               = "${var.environment}_jenkins_asg"
+    propagate_at_launch = "true"
+  }
+
+  tag {
+    key                 = "environment"
+    value               = "${var.environment}"
+    propagate_at_launch = "true"
+  }
+
+  tag {
+    key                 = "orchestration"
+    value               = "${var.orchestration}"
+    propagate_at_launch = "true"
+  }
+}
---
+++ asg_schedule.tf
@@ -0,0 +1,22 @@
+# --------------------------
+# Create scheduled action to rebuild Jenkins host,
+# ensuring Jenkins is updated on a schedule.
+# --------------------------
+
+resource "aws_autoscaling_schedule" "scale_down" {
+  scheduled_action_name  = "scale_down"
+  min_size               = 0
+  max_size               = 0
+  desired_capacity       = 0
+  recurrence             = "${var.scale_down_cron}"
+  autoscaling_group_name = "${aws_autoscaling_group.jenkins.name}"
+}
+
+resource "aws_autoscaling_schedule" "scale_up" {
+  scheduled_action_name  = "scale_up"
+  min_size               = 1
+  max_size               = 1
+  desired_capacity       = 1
+  recurrence             = "${var.scale_up_cron}"
+  autoscaling_group_name = "${aws_autoscaling_group.jenkins.name}"
+}
