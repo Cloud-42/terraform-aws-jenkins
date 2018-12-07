5697c6c821399ae5f82a135f3351930e7fb836c6
Chris Morgan <chris.morgan@perspectum-diagnostics.com>
2018-12-07 15:40:37 +0000

    ALB components, initial add

---
+++ alb_target_group.tf
@@ -0,0 +1,20 @@
+# ---------------------------------------------
+# ALB target group
+# ---------------------------------------------
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "jenkins-${var.environment}-tg"
+  port     = "${var.svc_port}"
+  protocol = "${var.target_group_protocol}"
+  vpc_id   = "${var.vpc_id}"
+
+  health_check {
+    healthy_threshold   = "${var.healthy_threshold}"
+    unhealthy_threshold = "${var.unhealthy_threshold}"
+    timeout             = "${var.timeout}"
+    interval            = "${var.interval}"
+    matcher             = "${var.success_codes}"
+
+    path = "${var.target_group_path}"
+    port = "${var.target_group_port}"
+  }
+}
---
+++ alb_target_group_attachments.tf
@@ -0,0 +1,9 @@
+# ---------------------------------------------
+#  ALB target group attachment
+# ---------------------------------------------
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  depends_on = ["aws_autoscaling_group.jenkins"]
+
+  autoscaling_group_name = "${aws_autoscaling_group.jenkins.id}"
+  alb_target_group_arn   = "${aws_lb_target_group.alb_target_group.arn}"
+}
