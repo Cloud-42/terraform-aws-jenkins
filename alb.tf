9c800e2f3bc2d5d51101ec53c753d53506282ee7
Chris Morgan <chris.morgan@perspectum-diagnostics.com>
2018-12-07 15:43:17 +0000

    ALB initial add

---
+++ alb.tf
@@ -0,0 +1,22 @@
+#--------------------------------------------------------------------
+# Jenkins ALB
+#--------------------------------------------------------------------
+resource "aws_lb" "jenkins" {
+  # only hyphens are allowed in name 
+  name = "${var.environment}-jenkins-alb"
+
+  enable_cross_zone_load_balancing = "${var.enable_cross_zone_load_balancing}"
+  internal                         = "${var.internal}"
+  load_balancer_type               = "application"
+  security_groups                  = ["${aws_security_group.alb_sg.id}"]
+  subnets                          = ["${split(",",var.subnets)}"]
+
+  enable_deletion_protection = "${var.enable_deletion_protection}"
+
+  tags {
+    "Environment"   = "${var.environment}"
+    "Contact"       = "${var.contact}"
+    "Role"          = "${var.role}"
+    "Orchestration" = "${var.orchestration}"
+  }
+}
