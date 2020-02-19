6ba4e33e7d2a8441c4b9f8b949c428646181872c
Cloud-42 <hello@cloud42.io>
2020-02-19 16:33:23 +0000

    Updated example

--- examples/main.tf
+++ examples/main.tf
@@ -3,36 +3,72 @@
 # top level "control" repository.
 #
 module "jenkins" {
-  source = "git::https://github.com/Cloud-42/terraform-aws-jenkins.git"
+  source  = "Cloud-42/jenkins/aws"
+  version = "2.0.0"
 
-  instance_type           = "t2.medium"
-  trusted_security_groups = "sg-12345xy"
-  private_subnets         = "subnet-xxyyz,subnet-12345"
-  orchestration           = "https://github.com/Cloud-42/terraform-aws-jenkins.git"
-  key_name                = "${var.key_name}"
-  environment             = "prod"
-  availability_zones      = "${var.availability_zones}"
-  contact                 = "contact@domain.io"
-  zone_id                 = "${module.vpc.zone_id}"
-  vpc_id                  = "${module.vpc.vpc_id}"
-  domain_name             = "${var.dns_domain}"
-  vpc_zone_identifier     = "${element(split(",",module.vpc.subnets_private),1)}"
+  instance_type           = "t3a.medium"
+  iam_instance_profile    = module.jenkins-cross-account-access.profile.name
+  trusted_security_groups = module.bastion_sg.this_security_group_id
+  private_subnets         = ["${element(split(",", module.vpc.subnets_private), 0)}", "${element(split(",", module.vpc.subnets_private), 1)}"]
+  orchestration           = var.orchestration
+  key_name                = var.key_name
+  environment             = var.environment
+  availability_zones      = var.availability_zones
+  contact                 = var.contact
+  zone_id                 = module.vpc.zone_id
+  vpc_id                  = module.vpc.vpc_id
+  domain_name             = var.dns_domain
+  security_group_alb      = ["${module.jenkins_sg.this_security_group_id}"]
+  vpc_zone_identifier     = ["${element(split(",", module.vpc.subnets_private), 0)}", "${element(split(",", module.vpc.subnets_private), 1)}"]
   ami                     = "${data.aws_ami.ubuntuserver_ami.id}"
-
   #
   # EFS Vars
   #
-  private_subnet_a = "subnet-xxyyz"
-
-  private_subnet_b  = "subnet-12345"
-  subnet_a_ip_range = "10.2.0.0/25"
-  subnet_b_ip_range = "10.2.0.128/25"
-
+  private_subnet_a  = "${element(split(",", module.vpc.subnets_private), 0)}"
+  private_subnet_b  = "${element(split(",", module.vpc.subnets_private), 1)}"
+  subnet_a_ip_range = "${element(split(",", var.nonprd_private_subnets), 0)}"
+  subnet_b_ip_range = "${element(split(",", var.nonprd_private_subnets), 1)}"
   #
   # ALB Vars
   #
-  certificate_arn = "arn:aws:acm:us-east-1:1234567891:certificate/161897e5-15aw-43sw-1523-51b0f92d2b3a"
-
-  subnets           = "${module.vpc.subnets_public}"
+  certificate_arn   = module.certificate.cert.id
+  subnets           = module.vpc.subnets_public
   target_group_path = "/login"
 }
+
+# ---------------------
+# SG for Jenkins ALB
+# ---------------------
+module "jenkins_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "3.1.0"
+
+  name        = "jenkins_alb_sg"
+  description = "Security group granting access to the Jenkins endpoint"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "TCP"
+      description = "HTTP Access"
+      cidr_blocks = var.office_ip
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "TCP"
+      description = "HTTPS Access"
+      cidr_blocks = var.office_ip
+    },
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
